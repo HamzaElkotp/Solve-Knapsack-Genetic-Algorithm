07df8e51ea20b2aecb24e42bd03d4dd9de49fbf4
HamzaDev <hmzt25235@gmail.com>
2025-11-17 17:37:40 +0200

    First code

---
+++ main.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Chromosome;
+class Generation;
+class Population;
+int fitness_function(Chromosome solution);
+bool validate(Chromosome solution);
+
+int problem_size = 0;
+int n = problem_size;
+int weight_limit = 0;
+
+struct item {
+    string name;
+    int weight;
+    int value;
+};
+vector<item> items(problem_size);
+
+
+
+class Chromosome{
+public:
+    vector<bool> chromosome_string;
+    int fitness = 0;
+
+    void calc_chromosome_fitness(){
+        fitness = fitness_function(*this);
+    }
+
+    bool operator==(const Chromosome& other) const {
+        return this->chromosome_string == other.chromosome_string;
+    }
+
+    Chromosome() : chromosome_string(problem_size) {}
+};
+
+class Generation{
+public:
+    vector<Chromosome> chromosomes;
+    long long average_fitness = 0;
+
+    void calc_average(){
+        long long total = 0;
+        for(auto & chromosome : chromosomes){
+            total+= chromosome.fitness;
+        }
+        average_fitness = round(total/(long long)chromosomes.size());
+    }
+
+    void add_chromosome(Chromosome &solution){
+        chromosomes.push_back(solution);
+    }
+
+    Generation() : chromosomes(0) {}
+};
+
+class Population{
+public:
+    vector<Generation> generations;
+
+    void add_generation(Generation &solutions){
+        generations.push_back(solutions);
+    }
+
+    Population() : generations(0) {}
+};
+
+
+int fitness_function(Chromosome &solution){
+    int weight = 0;
+    int value = 0;
+    for(int i=0; i<n; i++){
+        if(solution.chromosome_string[i]){
+            weight += items[i].weight;
+            value += items[i].value;
+        }
+    }
+    return weight;
+}
+
+bool validate(Chromosome &solution, Generation &gen){
+    if(solution.fitness==0)
+        return false;
+    for(auto & chromosome : gen.chromosomes){
+        if(solution == chromosome)
+            return false;
+    }
+    // check redundunt;
+    return true;
+}
+
+
+
+
+
+int main() {
+    problem_size = 10;
+
+    return 0;
+}
